fe1dc3b08c50f04824abf800cfb62979b856fcb1
Manishakahar <manisha.kahar1998@gmail.com>
2021-11-28 12:55:44 +0530

    Added: Calculate Flip Coin and print percentage of Heads and Tails

---
+++ src/com/blz/basicPrograms/leap.java
@@ -0,0 +1,20 @@
+package com.blz.basicPrograms;
+
+import java.util.Scanner;
+
+public class leap {
+    public static void main(String[] args) {
+        System.out.println("Enter a 4 digit number: ");
+        Scanner scan = new Scanner(System.in);
+        int year = scan.nextInt();
+        if (year > 999 && year < 10000) {
+            if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+                System.out.println("Given year is a leap year");
+            } else {
+                System.out.println("Given year is not a leap year");
+            }
+        } else {
+            System.out.println("Enter a valid year");
+        }
+    }
+}

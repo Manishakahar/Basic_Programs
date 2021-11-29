ec2782e11f4e7269ecda8a0efb2f4447559455de
Manishakahar <manisha.kahar1998@gmail.com>
2021-11-29 13:07:10 +0530

    Added: Calculate Quotient and Remainder

---
+++ src/com/blz/basicPrograms/quotient.java
@@ -0,0 +1,11 @@
+package com.blz.basicPrograms;
+
+public class quotient {
+    public static void main(String[] args) {
+        int dividend = 20, divisor = 5;
+        int quotient = dividend / divisor;
+        int remainder = dividend % divisor;
+        System.out.println("Quotient = " + quotient);
+        System.out.println("Remainder = " + remainder);
+    }
+}

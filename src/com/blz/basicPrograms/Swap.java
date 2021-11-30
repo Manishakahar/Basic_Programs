deeabbc6b307308c413b3da2d7c07e968dfc0052
Manishakahar <manisha.kahar1998@gmail.com>
2021-11-30 16:07:33 +0530

    Refactor: Swappint two numbers.

--- src/com/blz/basicPrograms/Swap.java
+++ src/com/blz/basicPrograms/Swap.java
@@ -1,20 +1,21 @@
 package com.blz.basicPrograms;
 
 import java.util.Scanner;
+
 public class Swap {
     public static void main(String[] args) {
         int team;
         System.out.println("Enter the value of X and Y");
-        Scanner sc =new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         int x = sc.nextInt();
-        int y= sc.nextInt();
-        System.out.println("before swapping numbers: "+x +"  "+ y);
+        int y = sc.nextInt();
+        System.out.println("before swapping numbers: " + x + "  " + y);
         System.out.println("Value of x:" + x);
         System.out.println("Value of y:" + y);
-        team=x;
-        x=y;
-        y=team;
-        System.out.println("After swapping: "+x +"   " + y);
+        team = x;
+        x = y;
+        y = team;
+        System.out.println("After swapping: " + x + "   " + y);
         System.out.println("Value of x:" + x);
         System.out.println("Value of y:" + y);
     }

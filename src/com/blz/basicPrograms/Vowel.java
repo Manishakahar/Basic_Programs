a785f0a5ddd9319a8397cd514c76fca8b928e4ed
Manishakahar <manisha.kahar1998@gmail.com>
2021-11-30 16:15:23 +0530

    Refactor: Checking Alphabet is vowel or consonant.

--- src/com/blz/basicPrograms/Vowel.java
+++ src/com/blz/basicPrograms/Vowel.java
@@ -2,12 +2,14 @@ package com.blz.basicPrograms;
 
 public class Vowel {
     public static void main(String[] args) {
-        char ch = 'A';
+        char ch = 'a';
 
         if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+
             System.out.println(ch + " is vowel");
-        } else {
-            System.out.println(ch + " is consonant");
         }
+         else {
+            System.out.println(ch + " is consonant");
+         }
     }
 }
